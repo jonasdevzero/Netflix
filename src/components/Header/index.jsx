ca1d9493b6555c7fe818043281fd5bd2d04b82b6
jonasdevzero <jonasdevzero@gmail.com>
2020-09-21 16:30:05 -0300

    Reestructuring header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,28 +4,38 @@ import { Link as ReactRouterLink } from 'react-router-dom'
 import * as ROUTES from '../../constants/routes'
 
 import { Container, Background, Logo, ButtonLink, Feature, Title, Subtitle } from './styles'
-import SignUpForm from '../SignUpForm'
+import SignUpForm from '../EmailForm'
 
-function Header({ children, background = true }) {
+function Header({ children, bg = true }) {
     return (
-        background ?
-        <Background>
-            <Container>
-                <ReactRouterLink to={ROUTES.HOME}>
-                    <Logo src="/images/misc/logo.svg" alt="Netflix" />
-                </ReactRouterLink>
-                <ButtonLink to={ROUTES.SIGN_IN}>
-                    Sign In
+        bg ?
+            <Background>
+                <Container>
+                    <ReactRouterLink to={ROUTES.HOME}>
+                        <Logo src="/images/misc/logo.svg" alt="Netflix" />
+                    </ReactRouterLink>
+                    <ButtonLink to={ROUTES.SIGN_IN}>
+                        Sign In
                 </ButtonLink>
-            </Container>
-            <Feature>
-                <Title>Unlimited films, TV programmes and more.</Title>
-                <Subtitle>Watch any where, Cancel at any time.</Subtitle>
-                <SignUpForm />
-            </Feature>
-        </Background>
-        :
-        children
+                </Container>
+                <Feature>
+                    <Title>Unlimited films, TV programmes and more.</Title>
+                    <Subtitle>Watch any where, Cancel at any time.</Subtitle>
+                    <SignUpForm />
+                </Feature>
+            </Background>
+            :
+            <Background>
+                <Container>
+                    <ReactRouterLink to={ROUTES.HOME}>
+                        <Logo src="/images/misc/logo.svg" alt="Netflix" />
+                    </ReactRouterLink>
+                    <ButtonLink to={ROUTES.SIGN_IN}>
+                        Sign In
+                </ButtonLink>
+                </Container>
+                { children }
+            </Background>
     )
 }
 
