ac861ee8c5e30312d1a40d8f0a98b57fdeb3df99
jonasdevzero <jonasdevzero@gmail.com>
2020-09-23 14:31:01 -0300

    Protecting routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
 
 import * as ROUTES from './constants/routes'
 
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes'
 import { Home, Browse, Signin, Signup } from './pages'
 
 function App() {
+  const user = null
+  
   return (
     <Router>
-      <Route exact path={ROUTES.HOME} component={Home} />
-      <Route exact path={ROUTES.BROWSE} component={Browse} />
-      <Route exact path={ROUTES.SIGN_IN} component={Signin} />
-      <Route exact path={ROUTES.SIGN_UP} component={Signup} />
+      <Switch>    
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
+          <Signin />
+        </IsUserRedirect>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
+          <Signup />
+        </IsUserRedirect>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect exact user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
+          <Home />
+        </IsUserRedirect>
+      </Switch>
+
     </Router>
   );
 }
