7e2d73a02a984d8fdf4712ac9f3412148e2f03a7
jonasdevzero <jonasdevzero@gmail.com>
2020-09-24 23:48:28 -0300

    adding Profiles component

---
+++ src/components/Profiles/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import Header from '../Header'
+import { Container, Title, List, Item, Picture, Name } from './styles'
+
+function Profiles({ user, setProfile }) {
+    return (
+        <>
+            <Header bg={false} />
+            <Container>
+                <Title>Who´s watching?</Title>
+                <List>
+                    <Item onClick={_ => setProfile({ displayName: user.displayName, photoURL: user.photoURL })}>
+                        <Picture src={user.photoURL ? `/images/users/${user.photoURL}.png` : '/images/misc/loading.gif'} />
+                        <Name>{user.displayName}</Name>
+                    </Item>
+                </List>
+            </Container>
+        </>
+    )
+}
+
+export default Profiles
