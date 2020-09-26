433dedf9a246b34934b7cd68f87489720b066afc
jonasdevzero <jonasdevzero@gmail.com>
2020-09-25 23:27:23 -0300

    rebuilding header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,75 +4,37 @@ import { Link as ReactRouterLink } from 'react-router-dom'
 import * as ROUTES from '../../constants/routes'
 
 import {
-        Container,
-        Background,
-        Logo,
-        ButtonLink,
-        Feature,
-        Title,
-        Subtitle,
-        FilmFeature,
-        FeatureCallOut,
-        Text,
-        Link,
-        Group,
-        Picture,
-        Dropdown,
-        Profile,
-        Search,
-        SearchIcon,
-        SearchInput,
-        PlayButton
-    } from './styles'
+    Container,
+    Background,
+    Logo,
+    ButtonLink,
+    Feature,
+    Title,
+    Subtitle,
+    FilmFeature,
+    FeatureCallOut,
+    Text,
+    Link,
+    Group,
+    Picture,
+    Dropdown,
+    Profile,
+    Search,
+    SearchIcon,
+    SearchInput,
+    PlayButton
+} from './styles'
 
 import SignUpForm from '../EmailForm'
 
-function Header({ children, bg = true, src, browser = false, user, firebase, search, setSearch }) {
+function Header({ children, bg = true, src, browser = false, user, firebase, search, setSearch, category, setCategory }) {
     const [searchActive, setSearchActive] = useState(false)
 
     return (
         browser ?
             (
                 <Background src={src} smallView>
-                    <Container>
-                        <Group>
-                            <ReactRouterLink to={ROUTES.HOME}>
-                                <Logo src="/images/misc/logo.svg" alt="Netflix" />
-                            </ReactRouterLink>
-                            <Link>Series</Link>
-                            <Link>Films</Link>
-                        </Group>
-                        <Group>
-                            <Search>
-                                <SearchIcon onClick={_ => setSearchActive(!searchActive)}>
-                                    <img src="/images/icons/search.png" alt="Search"/>
-                                </SearchIcon>
-                                <SearchInput value={search} onChange={e => setSearch(e.target.value)} placeholder="Search" active={searchActive} />
-                            </Search>
-                            <Profile>
-                                <Picture src={user.photoURL} />
-                                <Dropdown>
-                                    <Group>
-                                        <Picture src={user.photoURL} />
-                                        <Link>{user.displayName}</Link>
-                                    </Group>
-                                    <Group>
-                                        <Link onClick={_ => firebase.auth().signOut()}>Sing out</Link>
-                                    </Group>
-                                </Dropdown>
-                            </Profile>
-                        </Group>
-                        {children}
-                    </Container>
-                    <FilmFeature>
-                        <FeatureCallOut>Watch Joker Now</FeatureCallOut>
-                        <Text>
-                            Forever alone in a crowd, failed comedian Arthur Fleck seeks connection as he walks the streets of Gotham
-                            City. Arthur wears two masks -- the one he paints for his day job as a clown, and the guise he projects in a
-                            futile attempt to feel like he's part of the world around him.
-                            </Text>
-                            <PlayButton>Play</PlayButton>
-                    </FilmFeature>
+                    {children}
                 </Background>
             )
             :
@@ -106,6 +68,58 @@ function Header({ children, bg = true, src, browser = false, user, firebase, sea
 
 
     )
+}
+
+Header.Container = function HeaderContainer({ children, ...rest }) {
+    return <Container {...rest}>{children}</Container>
+}
+
+Header.Group = function HeaderGroup({ children, ...rest }) {
+    return <Group {...rest}>{children}</Group>
+}
+
+Header.Link = function HeaderLink({ children, ...rest }) {
+    return <Link {...rest}>{children}</Link>
+}
+
+Header.Search = function HeaderSearch({ children, ...rest }) {
+    return <Search {...rest}>{children}</Search>
+}
+
+Header.SearchIcon = function HeaderSearchIcon({ children, ...rest }) {
+    return <SearchIcon {...rest}>{children}</SearchIcon>
+}
+
+Header.SearchInput = function HeaderSearchInput({ children, ...rest }) {
+    return <SearchInput {...rest}>{children}</SearchInput>
+}
+
+Header.Profile = function HeaderProfile({ children, ...rest }) {
+    return <Profile {...rest}>{children}</Profile>
+}
+
+Header.Picture = function HeaderPicture({ children, ...rest }) {
+    return <Picture {...rest}>{children}</Picture>
+}
+
+Header.Dropdown = function HeaderDropdown({ children, ...rest }) {
+    return <Dropdown {...rest}>{children}</Dropdown>
+}
+
+Header.Feature = function HeaderFeature({ children, ...rest }) {
+    return <Feature {...rest}>{children}</Feature>
+}
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...rest }) {
+    return <FeatureCallOut {...rest}>{children}</FeatureCallOut>
+}
+
+Header.Text = function HeaderText({ children, ...rest }) {
+    return <Text {...rest}>{children}</Text>
+}
+
+Header.PlayButton = function HeaderPlayButton({ children, ...rest }) {
+    return <PlayButton {...rest}>{children}</PlayButton>
 }
 
 export default Header
